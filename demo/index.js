f6de447ef9f7a7745f0017b1be15c3f26ecd004a
Kagami Hiiragi <kagami@genshiken.org>
2018-02-17 14:57:27 +0300

    Add link to library in demo

--- demo/index.js
+++ demo/index.js
@@ -15,7 +15,7 @@ class Post extends React.Component {
       <article className="post">
         <h3 className="post__header">Example post</h3>
         <section className="post__body">
-          So here is a simple demo of vmsg library. Imagine this is a blog post or forum thread. Below you can leave text comments, as usual. But there is one more button: “Record”. If you press it vmsg library will open you a microphone recording form. Resulting record will automatically be encoded to MP3 so file won't weight too much. So you can easily share your voice messages even on mobile network and server needs to neither waste CPU time by encoding to MP3 by itself nor using a lot of disk space to store records.
+          So here is a simple demo of <a href="https://github.com/Kagami/vmsg">vmsg library</a>. Imagine this is a blog post or forum thread. Below you can leave text comments, as usual. But there is one more button: “Record”. If you press it vmsg library will open you a microphone recording form. Resulting record will automatically be encoded to MP3 so file won't weight too much. So you can easily share your voice messages even on mobile network and server needs to neither waste CPU time by encoding to MP3 by itself nor using a lot of disk space to store records.
         </section>
         <Comments />
       </article>
