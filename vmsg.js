db684936315b273782a7abaf5fe174490b91344c
Kagami Hiiragi <kagami@genshiken.org>
2018-03-05 22:18:53 +0300

    Use wasm-polyfill on broken iOS versions
    See: https://github.com/kripken/emscripten/issues/6042

--- vmsg.js
+++ vmsg.js
@@ -75,12 +75,25 @@ function inlineWorker() {
     return blob;
   }
 
+  // https://github.com/brion/min-wasm-fail
+  function testSafariWebAssemblyBug() {
+    const bin = new Uint8Array([0,97,115,109,1,0,0,0,1,6,1,96,1,127,1,127,3,2,1,0,5,3,1,0,1,7,8,1,4,116,101,115,116,0,0,10,16,1,14,0,32,0,65,1,54,2,0,32,0,40,2,0,11]);
+    const mod = new WebAssembly.Module(bin);
+    const inst = new WebAssembly.Instance(mod, {});
+    // test storing to and loading from a non-zero location via a parameter.
+    // Safari on iOS 11.2.5 returns 0 unexpectedly at non-zero locations
+    return (inst.exports.test(4) !== 0);
+  }
+
   onmessage = (e) => {
     const msg = e.data;
     switch (msg.type) {
     case "init":
       const { wasmURL, shimURL } = msg.data;
       Promise.resolve().then(() => {
+        if (self.WebAssembly && !testSafariWebAssemblyBug()) {
+          delete self.WebAssembly;
+        }
         if (!self.WebAssembly) {
           importScripts(shimURL);
         }
